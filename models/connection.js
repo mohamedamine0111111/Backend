b607fa8b4bb6f5d1546399c865c6e0a40a8dcb28
mohamed amine el andaloussi <andaloussi101@hotmail.fr>
2025-02-16 17:32:57 +0100

    Mise à jour

--- models/connection.js
+++ models/connection.js
@@ -1,23 +1,21 @@
 const mongoose = require('mongoose');
-
-const connectionString = 'mongodb+srv://andaloussi101:072680940Aa@cluster0.kzy4b78.mongodb.net/CleanSweetHome';
-
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
-
-const mongod = new MongoMemoryServer()
-
+// Connexion à MongoDB en mode test ou production
 if (process.env.NODE_ENV === 'test') {
+  const mongod = new MongoMemoryServer();
   mongod.start().then(() => {
     const uri = mongod.getUri();
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(uri)
+      .then(() => console.log('Test Database connected'))
+      .catch(error => console.error('Error connecting to Test Database:', error));
   });
 } else {
+  const connectionString = 'mongodb+srv://andaloussi101:072680940Aa@cluster0.kzy4b78.mongodb.net/CleanSweetHome';
 
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-    .catch(error => console.error(error));
+  mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+    .then(() => console.log('Database connected'))
+    .catch(error => console.error('Error connecting to Database:', error));
 }
 
 module.exports = mongoose;
-
